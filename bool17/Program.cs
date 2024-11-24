63cc4a4aa5862d6028295e8c5cfc1e48a3e1e332
Jengo2007 <zuyunovmagamed@gmail.com>
2024-11-24 11:55:04 +0300

    er

--- bool17/Program.cs
+++ bool17/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 int a =int.Parse(Console.ReadLine());
-bool result =a>=100&&a<=999 ;
+bool result =a>=100&&a<=999&& a%2 !=0 ;
 Console.WriteLine(result);
